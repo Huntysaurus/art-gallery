5e6189f70fe208266ca71b82c9411e89464452d0
Jacob Yaw <yawjacob@yahoo.com>
2022-07-28 21:02:41 -0500

    creating user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: 'huntysaurus', password_digest: 'password', image_url: "some_image_1", bio:"this is a story about my life")
+User.create(username: 'sammysam', password_digest: 'userpassword', image_url: "some_image_2", bio: "this is a story about my life")
+User.create(username: 'kareena63123', password_digest: 'passworduser', image_url: "some_image_3", bio: "this is a story about my life")
