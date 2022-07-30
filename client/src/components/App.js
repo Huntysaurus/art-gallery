c6d9f99d6a8b0f1fe7729b3b657f2d0b4a65ede4
Jacob Yaw <yawjacob@yahoo.com>
2022-07-29 19:18:30 -0500

    creating dummy header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './Home';
 import Login from './Login';
+import Header from './Header';
 
 function App() {
   return (
     <>
+      <Header/>
       <Home/>
       <Login/>
     </>
