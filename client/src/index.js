5c834cd944c76a15137c447c0cd7c25e2c831b99
Jacob Yaw <yawjacob@yahoo.com>
2022-07-29 20:10:05 -0500

    trying to use browser router and it's not working correctly

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import App from './components/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Routes>
+      <App />
+    </Routes>
+  </Router>,
   document.getElementById('root')
 );
