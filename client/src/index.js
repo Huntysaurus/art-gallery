953a3d988b1c25c192d9ca11236e7613ee1034df
Jacob Yaw <yawjacob@yahoo.com>
2022-07-31 12:19:00 -0500

    successfully getting browser router to work

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import App from './components/App';
 
 ReactDOM.render(
-  <Router>
-    <Routes>
-      <App />
-    </Routes>
-  </Router>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
