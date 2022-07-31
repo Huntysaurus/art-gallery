cc79b13219e3757a4569ccd2bd1e87de05fa74e6
Jacob Yaw <yawjacob@yahoo.com>
2022-07-31 15:44:21 -0500

    styling login form, adding controlled form functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from '../appStyles.module.css'
 
 function Login() {
 
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleLogin(e){
+        e.preventDefault()
+        console.log({username, password})
+    }
+
     return (
-        <div>
-            <h2 className={styles.title_name}>Login Here</h2>
-        </div>
+        <>
+            <div className={styles.wrapper}>
+                <h2 className={styles.title_name}>Login Here</h2>
+
+                <form onSubmit={handleLogin}>
+                    <div className={styles.form_field}>
+                        <label className={styles.label}>
+                            {'username: '}
+                            <input
+                                placeholder="enter username"
+                                value={username}
+                                onChange={(e)=>setUsername(e.target.value)} 
+                            />
+                            <br/>
+                        </label>
+                    </div>
+                    <div className={styles.form_field}>
+                        <label className={styles.label}>
+                            {'password: '} 
+                            <input
+                                placeholder="enter password"
+                                value={password}
+                                onChange={(e)=>setPassword(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <br/>
+                    <button className={styles.button} type="submit">Login</button>
+                </form>
+            </div>
+            <div className={styles.wrapper}>
+                <p className={styles.text}>No account? Sign up here!</p>
+                <button>Sign Up</button>
+            </div>
+        </>
     )
 }
 
