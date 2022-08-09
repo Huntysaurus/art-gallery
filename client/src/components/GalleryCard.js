f7dfefcf762d8473ce1ed6ba37280f77186266c8
Jacob Yaw <yawjacob@yahoo.com>
2022-08-08 20:52:04 -0500

    styling the clicked piece page

--- client/src/components/GalleryCard.js
+++ client/src/components/GalleryCard.js
@@ -1,15 +1,22 @@
 import React from "react";
+import styles from '../appStyles.module.css';
 
 function GalleryCard({ piece }) {
 
     return (
-        <div>
-            <h1>{piece.title}</h1>
-            <img src={piece.image} alt={piece.title}/>
-            <p>{piece.medium}</p>
-            <p>{piece.description}</p>
-            <p>{piece.worth}</p>
-            <p>{piece.user.username}</p>
+        <div className={styles.card_holder}>
+            <h1 className={styles.title_name}>{piece.title}</h1>
+            <img className={styles.card_page}
+                src={piece.image}
+                alt={piece.title}
+            />
+            <div className={styles.card_iw}>
+                <p>{piece.medium}</p>
+                <p className={styles.card_description} >{piece.description}</p>
+                <br/>
+            </div>
+            <p>valued at ${piece.worth}</p>
+            <p style={{textDecoration: 'underline'}}>creator: {piece.user.username}</p>
         </div>
     )
 }
