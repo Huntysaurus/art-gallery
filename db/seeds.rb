79835dae180e2ecf02effad055c05b17bf8e89b8
Jacob Yaw <yawjacob@yahoo.com>
2022-08-11 20:26:01 -0500

    creating gallery seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(username: 'huntysaurus', password_digest: 'password', image_url: "some_image_1", bio:"this is a story about my life")
-User.create(username: 'sammysam', password_digest: 'userpassword', image_url: "some_image_2", bio: "this is a story about my life")
-User.create(username: 'kareena63123', password_digest: 'passworduser', image_url: "some_image_3", bio: "this is a story about my life")
+
+Gallery.create!([{
+    name: "Ogden Contemporary Arts",
+    address: '455 25th St, Ogden, UT 84401'
+},
+{
+    name: "Soulard Art Gallery",
+    address: '2028 s 12th St, St. Louis, MO 63104'
+}, 
+{
+    name: 'Yale University Art Gallery',
+    address: "1111 Chapel St, New Haven, CT 06510"
+},
+{
+    name: 'The Sheldon Concert Hall',
+    address: '3648 Washington Ave, St. Louis, MO 63108'
+},
+{
+    name: 'Contemporary Art Museum of St. Louis',
+    address: '3750 Washington Ave, St. Louis, MO 63108'
+},
+{
+    name: 'Lehigh University Art Galleries',
+    address: '420 E Packer Ave, Bethlehem, PA 18015'
+}]
+)
